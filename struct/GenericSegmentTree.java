cf55c90927552fb859c1079ec7b79bdf76d01459
Andrey Zayakin <hydrastuff@gmail.com>
2020-07-26 17:45:20 +0300

    fix date.

--- struct/GenericSegmentTree.java
+++ struct/GenericSegmentTree.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
- * Created by hydra on 04.03.17.
+ * Created by hydra on 26.07.2020.
  */
 public abstract class GenericSegmentTree<T> {
     private final T[] func;
